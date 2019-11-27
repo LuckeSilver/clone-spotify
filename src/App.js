162d5c800ee85fb964981a94229cdad9f68fa488
Lucas Silva <37150645+LuckeSilver@users.noreply.github.com>
2019-11-27 18:02:18 -0300

    construção spotify

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React , { Fragment } from 'react';
 
 import GlobalStyle from './styles/global';
+import { Wrapper, Container } from './styles/components';
 
 import Sidebar from './components/Sidebar';
+import Player from './components/Player';
+import { Content } from './components/Sidebar/styles';
+
 
 const App = () => (
-  <Fragment>
+  <Wrapper>
     <GlobalStyle />
-    <Sidebar />
-  </Fragment>
+      <Container>
+      <Sidebar />
+    </Container>
+    <Player />
+  </Wrapper>
 );
 
 export default App;
---
+++ src/components/Player/index.js
@@ -0,0 +1,62 @@
+import React from 'react';
+
+import Slider from 'rc-slider';
+
+import { Current, Content, Volume, Playng, Controls } from './styles';
+
+import VolumeIcon from '../../assets/images/volume.svg';
+import ShuffleIcon from '../../assets/images/shuffle.svg';
+import BackwardIcon from '../../assets/images/backward.svg';
+import PlayIcon from '../../assets/images/play.svg';
+import PauseIcon from '../../assets/images/pause.svg';
+import ForwardIcon from '../../assets/images/forward.svg';
+import RepeatIcon from '../../assets/images/repeat.svg';
+
+const Player = () => (
+  <Content>
+    <Current>
+      
+      <img src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2017/12/Stargroves-album-cover.png?auto=format&q=60&fit=max&w=930" alt="Cover"/>
+
+      <div>
+        <span>Times Like These</span>
+        <small>Foo Fighters</small>
+      </div>
+    </Current>
+
+    <Playng>
+      <Controls>
+        <button>
+          <img src={ShuffleIcon} alt="Shuffle"/>
+        </button>
+        <button>
+          <img src={BackwardIcon} alt="BackwardIcon"/>
+        </button>
+        <button>
+          <img src={PlayIcon} alt="PlayIcon"/>
+        </button>
+        <button>
+          <img src={PauseIcon} alt="PauseIcon"/>
+        </button>
+        <button>
+          <img src={ForwardIcon} alt="ForwardIcon"/>
+        </button>
+        <button>
+          <img src={RepeatIcon} alt="RepeatIcon"/>
+        </button>
+      </Controls>
+    </Playng>
+
+    <Volume>
+      <img src={VolumeIcon} alt="Volume"/>
+      <Slider
+        railStyle={{ background: '#404040',borderRadius: 10 }}
+        trackStyle={{ background: '#fff',  }}
+        handleStyle={{ display: 'none' }}
+        // value={100}
+        />
+    </Volume>
+  </Content>
+);
+
+export default Player;
